5300979c70e996268b77b45a626bb2541b883373
Natnael Alemayehu <se.natnael.alemayehu@gmail.com>
2024-09-25 17:20:38 +0300

    defer function included

--- main.go
+++ main.go
@@ -6,17 +6,28 @@ import (
 	"os"
 )
 
+func getFile(name string) (*os.File, func(), error) {
+	file, err := os.Open(name)
+	if err != nil {
+		return nil, nil, err
+	}
+	return file, func() {
+		file.Close()
+	}, nil
+}
+
 func main() {
 	if len(os.Args) < 2 {
 		log.Fatal("no file specified")
 	}
-	f, err := os.Open(os.Args[1])
+	f, closer, err := getFile(os.Args[1])
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer f.Close()
+	defer closer()
 
 	data := make([]byte, 2048)
+
 	for {
 		count, err := f.Read(data)
 		os.Stdout.Write(data[:count])
